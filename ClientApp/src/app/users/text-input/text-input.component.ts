7c25c8d590ce70776c259ad05cf5b46d1d032645
Alexandru Nicoara <alexandru.nicoara@amdaris.com>
2022-05-11 15:23:11 +0300

    Fix text input component

--- ClientApp/src/app/users/text-input/text-input.component.ts
+++ ClientApp/src/app/users/text-input/text-input.component.ts
@@ -1,32 +1,56 @@
-import { AfterContentInit, Component, Input, OnInit} from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import {
+  MatFormField,
+  MatFormFieldControl,
+} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+  styleUrls: ['./text-input.component.css'],
 })
-export class TextInputComponent implements OnInit{
-
+export class TextInputComponent implements OnInit {
   @Input()
-  label: string ="";
+  label: string = '';
 
-  @Input()
-  parentForm!: FormGroup;
+  // @Input()
+  // parentForm!: FormGroup;
 
-  @Input()
-  fieldName!: string;
-  
-  constructor() { }
-  
-  get formField(): FormControl{
-    return this.parentForm?.get(this.fieldName) as FormControl;
-    
+  // @Input()
+  // fieldName!: string;
+
+  // @Input()
+  // control!: FormControl;
+
+  @ContentChild(MatFormFieldControl, { static: true })
+  public formFieldControl!: MatFormFieldControl<any>;
+
+  @ViewChild('materialFormField', { static: true })
+  public matFormField!: MatFormField;
+
+  constructor() {}
+
+  get formField(): FormControl {
+    // const asd = this.parentForm?.get(this.fieldName) as FormControl;
+    // console.log(this.formFieldControl);
+    // console.log(asd);
+    const asdd = this.formFieldControl.ngControl?.control as FormControl;
+    return asdd;
   }
-ngOnInit(): void {
-  
-  console.log(this.parentForm);
-  console.log(this.fieldName);
-  console.log(this.formField);
-}
+
+  ngOnInit(): void {
+    // console.log(this.parentForm);
+    // console.log(this.fieldName);
+    // console.log(this.formField);
+
+    this.matFormField._control = this.formFieldControl;
+  }
 }
--- ClientApp/src/app/users/user-form/user-form.component.ts
+++ ClientApp/src/app/users/user-form/user-form.component.ts
@@ -1,49 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder, FormControl } from '@angular/forms';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.css']
+  styleUrls: ['./user-form.component.css'],
 })
-export class UserFormComponent{
-  
-
+export class UserFormComponent {
   userForm = this.fb.group({
-    firstName: ['',[Validators.required, Validators.maxLength(25), Validators.minLength(3), Validators.pattern("^[a-zA-Z\s]+$")]],
-    lastName: ['',[Validators.required, Validators.maxLength(25), Validators.minLength(3), Validators.pattern("^[a-zA-Z\s]+$")]],
-    email: ['',[Validators.required, Validators.email]],
-    phone: ['',[Validators.required,Validators.pattern("^[0-9\-]+$")]],
-    address: ['',Validators.maxLength(50)]
+    firstName: [
+      '',
+      [
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.minLength(3),
+        Validators.pattern('^[a-zA-Zs]+$'),
+      ],
+    ],
+    lastName: [
+      '',
+      [
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.minLength(3),
+        Validators.pattern('^[a-zA-Zs]+$'),
+      ],
+    ],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required, Validators.pattern('^[0-9-]+$')]],
+    address: ['', Validators.maxLength(50)],
   });
 
- 
-  constructor(private usersService :UsersService, private fb:FormBuilder) {
-   }
+  constructor(private usersService: UsersService, private fb: FormBuilder) {}
 
-  onSubmit():void {
-    console.log(this.userForm)
+  onSubmit(): void {
+    console.log(this.userForm);
     this.usersService.post(this.userForm.value).subscribe();
   }
-  /*get firstName(){
-    return this.userForm.get('firstName')
-  }*/
 
-  get lastName(){
-    return this.userForm.get('lastName')
+  get firstName() {
+    return this.userForm.get('firstName') as FormControl;
   }
 
-  get email(){
-    return this.userForm.get('email')
+  get lastName() {
+    return this.userForm.get('lastName');
   }
 
-  get phone(){
-    return this.userForm.get('phone')
+  get email() {
+    return this.userForm.get('email');
   }
 
-  get address(){
-    return this.userForm.get('address')
+  get phone() {
+    return this.userForm.get('phone');
+  }
+
+  get address() {
+    return this.userForm.get('address');
   }
 }
 
